e638c981a607a719cfca4669adbaf407dca6a18e
daniel marinho <danielmarinhom@gmail.com>
2025-06-20 11:47:29 -0300

    fix: json ident

--- obj/Debug/net9.0/pam.AssemblyInfo.cs
+++ obj/Debug/net9.0/pam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365ece789c2307bbcab7ea5166969aac6a4494ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("pam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
